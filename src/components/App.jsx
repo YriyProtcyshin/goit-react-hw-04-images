5ca68f26a1b613ae76c990aff54ea756cd252b64
YriyProtcyshin <105303233+YriyProtcyshin@users.noreply.github.com>
2022-12-14 16:57:48 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react"
+
+const API = "https://pixabay.com/api/?key=30615642-0c3410a518698d6d783d2cae0"
+
+
+
+export class App extends Component{
+
+  componentDidMount(){
+    fetch(API).then(res=>res.json()).then(resonse=> console.log(resonse))
+  }
+  
+  render(){
+    
+    return <div>Hello</div>
+  }
+}
+
+
