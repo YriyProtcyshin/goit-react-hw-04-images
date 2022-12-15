5411b1aea8e245422185b9e6c8e6437afdca60ca
YriyProtcyshin <105303233+YriyProtcyshin@users.noreply.github.com>
2022-12-15 18:58:07 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,30 @@
 import { Component } from "react"
+import { ImageGalery } from "./ImageGallery/ImageGallery"
 
 const API = "https://pixabay.com/api/?key=30615642-0c3410a518698d6d783d2cae0"
 
 
 
+
+
 export class App extends Component{
+  state = {
+    items: [],
+    isLoding:false
+  }
 
   componentDidMount(){
-    fetch(API).then(res=>res.json()).then(resonse=> console.log(resonse))
+    this.setState({isLoding: true})
+    fetch(API)
+    .then(res=>res.json())
+    .then(resonse=> this.setState({
+      items: resonse.hits
+    }))
+    .finally(this.setState({isLoding: false}))
   }
   
-  render(){
-    
-    return <div>Hello</div>
+  render(){    
+    return <ImageGalery items= {this.state.items}/>
   }
 }
 
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,9 @@
+import css from "./ImageGallery.module.css"
+
+export function ImageGalery({items}){    
+    return (        
+        <>
+        {items.map(item => <img  key={item.id} src={item.previewURL} alt={item.tags} width="200px" height="150px"/>)}
+        </>
+    )    
+}
