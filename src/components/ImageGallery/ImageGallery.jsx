9450c62a379a35a8e2194857024f8e62d7c03074
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-12-15 19:03:14 +0200

    commit

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,17 @@
-import css from "./ImageGallery.module.css"
+// import css from "./ImageGallery.module.css"
 
-export function ImageGalery({items}){    
-    return (        
-        <>
-        {items.map(item => <img  key={item.id} src={item.previewURL} alt={item.tags} width="200px" height="150px"/>)}
-        </>
-    )    
+export function ImageGalery({ items }) {
+  return (
+    <>
+      {items.map(item => (
+        <img
+          key={item.id}
+          src={item.previewURL}
+          alt={item.tags}
+          width="200px"
+          height="150px"
+        />
+      ))}
+    </>
+  );
 }
