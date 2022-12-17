ffc35678b0d2822fa795183fd28915542e8e7da6
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-12-17 23:12:26 +0200

    commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,7 +8,7 @@ export function Loader() {
         height="80"
         width="80"
         radius="9"
-        color="#4050b5"
+        color="#f50505"
         ariaLabel="three-dots-loading"
         visible={true}
       />
