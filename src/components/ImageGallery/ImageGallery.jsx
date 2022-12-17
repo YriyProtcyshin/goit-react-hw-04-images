4b46d2909da205f89deb5d00aa1d5e5b21249a61
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-12-17 19:20:40 +0200

    commit

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,16 +1,12 @@
 // import css from "./ImageGallery.module.css"
 
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
 export function ImageGalery({ items }) {
   return (
     <>
       {items.map(item => (
-        <img
-          key={item.id}
-          src={item.previewURL}
-          alt={item.tags}
-          width="200px"
-          height="150px"
-        />
+        <img key={item.id} src={item.previewURL} alt={item.tags} />
       ))}
     </>
   );
