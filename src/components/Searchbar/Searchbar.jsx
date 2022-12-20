94a14fdc03321fe9f0fafcf584e73d64171058b9
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-12-20 23:48:01 +0200

    commit

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,43 +1,39 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './Searchbar.module.css';
 import { AiOutlineSearch } from 'react-icons/ai';
 
-export class Searchbar extends Component {
-  state = {
-    input: '',
-  };
+export function Searchbar({ onSubmit }) {
+  const [input, setInput] = useState('');
 
-  handleInput = event => {
-    event.preventDefault();
-    if (this.state.input.trim() !== '') {
-      this.props.onSubmit(this.state.input.trim());
-      this.setState({ input: '' });
-    }
-  };
-
-  handleChange = e => {
-    this.setState({ input: e.target.value.toLowerCase() });
-  };
-
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleInput}>
-          <button type="submit" className={css.SearchForm_button}>
-            <AiOutlineSearch />
-            <span className={css.SearchForm_button_label}>Search</span>
-          </button>
-          <input
-            className={css.SearchForm_input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.input}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
+  function handleChange(e) {
+    setInput(e.target.value.toLowerCase());
   }
+
+  function handleInput(event) {
+    event.preventDefault();
+    if (input.trim() !== '') {
+      onSubmit(input.trim());
+      setInput('');
+    }
+  }
+
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleInput}>
+        <button type="submit" className={css.SearchForm_button}>
+          <AiOutlineSearch />
+          <span className={css.SearchForm_button_label}>Search</span>
+        </button>
+        <input
+          className={css.SearchForm_input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={input}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
 }
