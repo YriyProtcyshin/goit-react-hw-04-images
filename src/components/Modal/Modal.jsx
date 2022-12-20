2384a238d336236604942379f1552148d0a64315
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-12-21 00:04:39 +0200

    commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,76 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 import { SlClose } from 'react-icons/sl';
 
 const modalRoot = document.querySelector('#modal_root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
+export function Modal({ activeItem, onCloseModal }) {
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
 
-  handleKeyDown = event => {
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  function handleKeyDown(event) {
     if (event.code === 'Escape') {
-      this.props.onCloseModal();
+      onCloseModal();
     }
-  };
-
-  handBackDropClick = event => {
-    if (event.currentTarget === event.target) {
-      this.props.onCloseModal();
-    }
-  };
-
-  render() {
-    const { onCloseModal, activeItem } = this.props;
-    return createPortal(
-      <div className={css.Overlay} onClick={this.handBackDropClick}>
-        <button type="button" className={css.button} onClick={onCloseModal}>
-          <SlClose />
-        </button>
-        <div className={css.Modal}>
-          <img src={activeItem.largeImageURL} alt={activeItem.tags} />
-        </div>
-      </div>,
-      modalRoot
-    );
   }
+
+  function handBackDropClick(event) {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
+    }
+  }
+
+  return createPortal(
+    <div className={css.Overlay} onClick={handBackDropClick}>
+      <button type="button" className={css.button} onClick={onCloseModal}>
+        <SlClose />
+      </button>
+      <div className={css.Modal}>
+        <img src={activeItem.largeImageURL} alt={activeItem.tags} />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.handleKeyDown);
+//   }
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onCloseModal();
+//     }
+//   };
+
+//   handBackDropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onCloseModal();
+//     }
+//   };
+
+//   render() {
+//     const { onCloseModal, activeItem } = this.props;
+//     return createPortal(
+//       <div className={css.Overlay} onClick={this.handBackDropClick}>
+//         <button type="button" className={css.button} onClick={onCloseModal}>
+//           <SlClose />
+//         </button>
+//         <div className={css.Modal}>
+//           <img src={activeItem.largeImageURL} alt={activeItem.tags} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
